8fe7d737fe2714b2ebe2dd820719a389a025b1e7
kxzazz <aman642.g@gmail.com>
2026-09-20 21:26:11 +0530

    LC11

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,13 +1,28 @@
 package arrays
 
-func TwoSum(arr []int, target int) [2]int {
+func twoSum(arr []int, target int) [2]int {
 	idx_map := make(map[int]int)
 	for i := range len(arr) {
 		if _, found := idx_map[arr[i]]; found {
-			return [2]int{i, idx_map[arr[i]]}
+			return [2]int{idx_map[arr[i]], i}
 		}
 		idx_map[target-arr[i]] = i
 
 	}
 	return [2]int{-1, -1}
+}
+
+func maxArea(height []int) int {
+	p, q := 0, len(height)-1
+	max_area := 0
+	for p < q {
+		area := min(height[p], height[q]) * (q - p)
+		max_area = max(max_area, area)
+		if height[p] > height[q] {
+			q--
+		} else {
+			p++
+		}
+	}
+	return max_area
 }
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -9,7 +9,7 @@ func TestTwoSum(t *testing.T) {
 		arr := []int{2, 7, 11, 15}
 		target := 9
 		expected := []int{0, 1}
-		result := TwoSum(arr, target)
+		result := twoSum(arr, target)
 		if result[0] != expected[0] || result[1] != expected[1] {
 			t.Errorf("Expected %v, got %v", expected, result)
 		}
@@ -19,8 +19,27 @@ func TestTwoSum(t *testing.T) {
 		arr := []int{5, 1, 2, 3, 4}
 		target := 77
 		expected := []int{-1, -1}
-		result := TwoSum(arr, target)
+		result := twoSum(arr, target)
 		if result[0] != expected[0] || result[1] != expected[1] {
+			t.Errorf("Expected %v, got %v", expected, result)
+		}
+	})
+}
+
+func TestMaxArea(t *testing.T) {
+	t.Run("Example 1 - Leetcode", func(t *testing.T) {
+		arr := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+		expected := 49
+		result := maxArea(arr)
+		if result != expected {
+			t.Errorf("expected %v, got %v", expected, result)
+		}
+	})
+	t.Run("Example 2 - Leetcode", func(t *testing.T) {
+		arr := []int{1, 1}
+		expected := 1
+		result := maxArea(arr)
+		if result != expected {
 			t.Errorf("Expected %v, got %v", expected, result)
 		}
 	})
