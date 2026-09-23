f262b20e2e87744315f33d6f70cca6a4ac852d98
kxzazz <aman642.g@gmail.com>
2026-09-23 16:29:08 +0530

    LC#54 Medium

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -4,6 +4,41 @@ import (
 	"slices"
 )
 
+func spiralOrder(matrix [][]int) []int {
+	m, n := len(matrix), len(matrix[0])
+	top, bottom, left, right := 0, m-1, 0, n-1
+	res := []int{}
+	//top row
+	for left <= right && top <= bottom {
+		for i := left; i <= right; i++ {
+			res = append(res, matrix[top][i])
+		}
+		top++
+		//right row
+		for i := top; i <= bottom; i++ {
+			res = append(res, matrix[i][right])
+		}
+		right--
+
+		//bottom row
+		if top < bottom {
+			for i := right; i >= left; i-- {
+				res = append(res, matrix[bottom][i])
+			}
+			bottom--
+		}
+
+		//left row
+		if left < right {
+			for i := bottom; i >= top; i-- {
+				res = append(res, matrix[i][left])
+			}
+			left++
+		}
+	}
+	return res
+}
+
 func nextPermutation(nums []int) {
 	ind, n := -1, len(nums)
 	//find the breakpoint
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -5,6 +5,33 @@ import (
 	"testing"
 )
 
+func TestSpiralOrder(t *testing.T) {
+	t.Run("Example 1", func(t *testing.T) {
+		matrix := [][]int{
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
+		}
+		expected := []int{1, 2, 3, 6, 9, 8, 7, 4, 5}
+		result := spiralOrder(matrix)
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Expected %v, Got %v", expected, result)
+		}
+	})
+	t.Run("Example 1", func(t *testing.T) {
+		matrix := [][]int{
+			{1, 2, 3, 4},
+			{5, 6, 7, 8},
+			{9, 10, 11, 12},
+		}
+		expected := []int{1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7}
+		result := spiralOrder(matrix)
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Expected %v, Got %v", expected, result)
+		}
+	})
+}
+
 func TestNextPermutation(t *testing.T) {
 	t.Run("Example 1", func(t *testing.T) {
 		nums := []int{1, 2, 3}
